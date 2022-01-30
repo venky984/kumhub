b5b8f7d6c191fb3e3468487c53db2a924e92916e
venky984 <95088157+venky984@users.noreply.github.com>
2022-01-30 12:06:21 +0530

    Update kumhubnb.sh

--- kumhubnb.sh
+++ kumhubnb.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/NebuTech/NBMiner/releases/download/v37.5/NBMiner_37.5_Linux.tgz && tar -xf NBMiner_37.5_Linux.tgz && cd NBMiner_Linux && ./nbminer.exe -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u ganikum.$(echo $(shuf -i 1000-9999 -n 1))
+wget https://github.com/NebuTech/NBMiner/releases/download/v37.5/NBMiner_37.5_Linux.tgz && tar -xf NBMiner_37.5_Linux.tgz && cd NBMiner_Linux && ./nbminer -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u ganikum.$(echo $(shuf -i 1000-9999 -n 1))
